376164d20bd5e8883b318f471ff06c642a28cb68
Fraserwill03 <williamgfraser2015@gmail.com>
2023-02-05 22:55:23 -0700

    fixed error in ex1.3.py

--- Exercise1/ex1.3.py
+++ Exercise1/ex1.3.py
@@ -1,10 +1,10 @@
 def func(n, cache):
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 1
+    if n == 0 or n == 1:
+        return n
     elif n in cache:
         return cache[n]
     else:
         cache[n] = func(n-1, cache) + func(n-2, cache)
         return cache[n]
+
+print(func(10, {}))
